81c42bade7ce53f7e5e2779ea8b25c85fc47b84d
Dries Vanmierlo <dries.vanmierlo@student.ehb.be>
2021-03-20 14:16:31 +0100

    Made possible to post message and delete token

--- js/index.js
+++ js/index.js
@@ -2,16 +2,50 @@
 
 const messageSystem = {
   startFetching() {
+    setInterval(this.fetchMessages, 3000);
+    //console.log(this.startFetching);
   },
 
   sendMessage(msg) {
     // https://thecrew.cc/api/message/create.php?token=__TOKEN__ POST
+    fetch(`https://thecrew.cc/api/message/create.php?token=${userSystem.token}`, {
+        method: "POST",
+        body: JSON.stringify({
+          message: msg
+        })
+      })
+      .then((response) => {
+        return response.json();
+      });
   },
 
   fetchMessages() {
-    // https://thecrew.cc/api/message/read.php?token=__TOKEN__ GET
+    console.log("Fetch!");
+    fetch(`https://thecrew.cc/api/message/read.php?token=${userSystem.token}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const output = document.getElementById("output");
+        let htmlString = "";
+        data.forEach((message) => {
+          htmlString += `<div id="output">
+          <div class="message">
+            <span class="by">${message.handle}</span>
+            <span class="on">${message.created_at}</span>
+            <p>${message.message}</p>
+          </div>`;
+        });
+
+        output.innerHTML = htmlString;
+      });
+    //const output = document.getElementById('output');
+    //output.insertAdjacentHTML('beforeend', htmlString);
   }
 };
+
+// https://thecrew.cc/api/message/read.php?token=__TOKEN__ GET
 
 const userSystem = {
   token: "",
@@ -25,22 +59,91 @@ const userSystem = {
     return localStorage.getItem("token");
   },
 
-  logout() { 
+  logout() {
     localStorage.removeItem("token");
+    this.updateUser();
   },
 
   login(email, password) {
-    // https://thecrew.cc/api/user/login.php POST
+    fetch("https://thecrew.cc/api/user/login.php", {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.token);
+        this.token = data.token;
+        this.saveToken();
+        document.getElementById("loginWindow").style.display = "none";
+      });
   },
 
   updateUser(password, handle) {
     // https://thecrew.cc/api/user/update.php?token=__TOKEN__ POST
+    fetch(`https://thecrew.cc/api/user/update.php?token=${userSystem.token}`, {
+        method: "POST",
+        body: JSON.stringify({
+          password: password,
+          handle: handle
+        })
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.token);
+        this.token = data.token;
+        this.saveToken();
+        document.getElementById("loginWindow").style.display = "none";
+      });
   }
 };
 
 const display = {
   initFields() {
-  }
+    const form = document.getElementById("loginForm");
+    form.addEventListener("submit", this.submitHandler);
+
+    const messageForm = document.getElementById("messageForm");
+    messageForm.addEventListener("submit", this.submitMessage);
+
+    const logoutButton = document.getElementById("logoutBtn");
+    logoutButton.addEventListener('click', userSystem.logout);
+  },
+
+  submitHandler(e) {
+    e.preventDefault("submit");
+    let loginEmail = document.getElementById("emailField").value;
+    let loginPassword = document.getElementById("passwordField").value;
+    console.log(loginEmail, loginPassword);
+    userSystem.login(loginEmail, loginPassword);
+    messageSystem.startFetching();
+  },
+
+  submitMessage(e) {
+    e.preventDefault("submit");
+    let messageText = document.getElementById("messageField").value;
+    console.log(messageText);
+    messageSystem.sendMessage(messageText);
+  },
+
+  render() {
+    console.log("render");
+    const tokenVar = userSystem.getToken();
+    if (tokenVar) {
+      console.log("logged in");
+      document.getElementById("loginWindow").style.display = "none";
+      messageSystem.startFetching();
+    } else {
+      console.log("not logged in");
+    }
+  },
+  renderMessages() {}
 };
 
-  display.initFields();
+display.initFields();
